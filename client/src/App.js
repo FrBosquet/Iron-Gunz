dca1a94ab09ef4ff71f676e41df9bf4cdb60ac8f
Fran Bosquet <fran@redradix.com>
2017-12-19 18:21:06 +0100

    Clean base component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import io from 'socket.io-client'
+import Logger from './modules/logger/LoggerConnected.jsx'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.socket = io('192.168.1.45:4343')
-    this.state = { message: ''}
-
-    document.addEventListener('keydown', keyEvent => {
-      this.socket.emit('keyPress', keyEvent.key)
-    })
-  }
-
-  componentDidMount(){
-    this.socket.on('notification',console.log)
-    this.socket.on('message', msg => this.setState({ message: msg }))
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-          {this.state.message}
-        </div>
+        <Logger />
       </div>
     )
   }
