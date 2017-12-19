7759ec9b4b9b0d0a63585591888415add8feb595
Fran Bosquet <fran@redradix.com>
2017-12-19 18:22:41 +0100

    Add server basic notification system

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 let state = ''
+let rooms = {
+  room1: {},
+  room2: {},
+  room3: {},
+  room4: {}
+}
 
 server.listen(4343, () => {
   console.log('Listening on port 4343')
@@ -11,28 +17,8 @@ server.listen(4343, () => {
 
 io.on('connection', socket => {
   console.log('Client connected', socket.id)
-
-  socket.join('A game room')
-  socket.emit('message', state)
-  io.to('A game room').emit('notification', `${socket.id} signed to this room`)
-  
-  
-  socket.on('keyPress', key => {
-    
-    switch(key){
-      case 'Backspace':
-        state = state.substr(0, state.length - 1)
-        break
-      case 'Shift':
-      case 'Enter':
-      case 'Tab':
-      case 'Delete':
-        break
-      default:
-        state = `${state}${key}`
-    }
-    io.to('A game room').emit('message', state)
-  })
+  socket.emit('MESSAGE', 'Welcome to the server')
+  io.emit('MESSAGE', `${socket.id} has connected to the server`)
 
   socket.on('disconnect', () => io.to('A game room').emit('notification', `${socket.id} has left this room`))
 })
