13d271248be5b0736a0aeb251f134556edfe84d9
Fran Bosquet <fran@redradix.com>
2017-12-20 16:50:13 +0100

    Add presentational box

--- client/src/modules/logger/Logger.jsx
+++ client/src/modules/logger/Logger.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './style.css'
+import Box from '../shared/box'
 
 const Logger = ({logs}) =>
-  <div className="logger">
+  <Box>
     <h1 className="logger-title">Logger</h1>
     {logs.map(log => <div key={log}>{log}</div>)}
-  </div>
+  </Box>
 
 export default Logger
---
+++ client/src/modules/shared/box/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+
+const Box = ({ children }) =>
+  <div className='box'>
+    { children }
+  </div>
+
+export default Box
