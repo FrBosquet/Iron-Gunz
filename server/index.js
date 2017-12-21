6993b5a6407798398a17c78f8feb0d4586990e69
Fran Bosquet <fran@redradix.com>
2017-12-21 09:10:40 +0100

    Add verbs for room retrieve and join

--- server/index.js
+++ server/index.js
@@ -20,5 +20,17 @@ io.on('connection', socket => {
   socket.emit('MESSAGE', 'Welcome to the server')
   io.emit('MESSAGE', `${socket.id} has connected to the server`)
 
+  socket.on('RETRIEVE_ROOMS', () =>{
+    console.log(`${socket.id} retrieves list of rooms`)
+    const roomList = Object.keys(rooms)
+    socket.emit('ROOM_LIST', roomList)
+  })
+
+  socket.on('JOIN_ROOM', room => {
+    const msg = `${socket.id} joins ${room}`
+    console.log(msg)
+    io.emit('MESSAGE', msg)
+  })
+
   socket.on('disconnect', () => io.to('A game room').emit('notification', `${socket.id} has left this room`))
 })
