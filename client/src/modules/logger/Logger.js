61210eb666814769c790db86de483ca2d7d55054
Fran Bosquet <fran@redradix.com>
2017-12-21 18:31:42 +0100

    Cosmetic changes :art:

--- client/src/modules/logger/Logger.js
+++ client/src/modules/logger/Logger.js
@@ -5,8 +5,8 @@ import { Box } from '../shared'
 const Logger = ({logs}) =>
   <Box width='50%'>
     <h1 className="logger-title">Logger</h1>
-    <Box className="log-screen">
-      {logs.reverse().map(log => <div key={log}>{log}</div>)}
+    <Box className="green-phospor">
+      {logs.map(log => <div key={log}>{log}</div>)}
     </Box>
   </Box>
 
