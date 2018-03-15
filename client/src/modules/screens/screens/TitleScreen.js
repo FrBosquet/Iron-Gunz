a325ec6271cea1d4842fbf17b4f354846bb3f33d
Fran Bosquet <fran@redradix.com>
2018-03-15 17:28:42 +0100

    Update title screen styling to use new shared components

--- client/src/modules/screens/screens/TitleScreen.js
+++ client/src/modules/screens/screens/TitleScreen.js
@@ -4,12 +4,12 @@ import { Title, LayoutWrapper, Button } from '../../shared'
 
 export const TitleScreen = ({goToLobby, goToOptions, goToAbout}) => 
   <Screen>
-    <LayoutWrapper weight={1} align='start'>
+    <LayoutWrapper weight={1} align='start' padded>
       <Title shadow={true} size='enormous'>IRONGUNZ</Title>
     </LayoutWrapper>
-    <LayoutWrapper weight={1} direction='horizontal' align='end' justify='end'>
-      <Button onClick={goToLobby}> Start </Button>
-      <Button onClick={goToOptions}> Options </Button>
-      <Button onClick={goToAbout}> About </Button>
+    <LayoutWrapper weight={1} direction='horizontal' align='end' justify='end' padded>
+      <Button onClick={goToLobby} big> Start </Button>
+      <Button onClick={goToOptions} big> Options </Button>
+      <Button onClick={goToAbout} big> About </Button>
     </LayoutWrapper>
   </Screen>
