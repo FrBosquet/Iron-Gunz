678f67c2e3d2241fde9c86f6a89305b60abe4ff0
Fran Bosquet <fran@redradix.com>
2018-05-13 10:39:33 +0200

    :rotating_light::art:

--- server/lib/state/__test__/Client.test.js
+++ server/lib/state/__test__/Client.test.js
@@ -2,27 +2,28 @@ const Client = require('../Client')
 const { expect } = require('chai')
 
 describe('Client test', () => {
+	let client
 	const id = 39485
 	const keyset = [45, 12, 35]
 
+	beforeEach(() => {
+		client = new Client(id)
+	})
+
 	it('Should create a client', () => {
-		const client = new Client(id)
 		expect(client).to.exist
 	})
 
 	it('Should have a provided id', () => {
-		const client = new Client(id)
 		expect(client.id).to.be.equal(id)
 	})
 
 	it('Should have a keyset', () => {
-		const client = new Client(id)
 		expect(client.keyset).to.exist
 		expect(client.keyset).to.be.instanceof(Array)
 	})
 
 	describe('Should expose keyset getter and setter', () => {
-		const client = new Client(id)
 		it('Getter', () => {
 			expect(client.getKeyset).to.exist
 			expect(client.getKeyset()).to.deep.equals(client.keyset)
